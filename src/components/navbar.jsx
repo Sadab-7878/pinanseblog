61caee0f0412660c62abfbdb3b8a800a710598e5
Sadab76 <sadabuzzaman75@gmail.com>
2022-11-29 19:44:49 +0600

    gdusg

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -16,14 +16,14 @@ const navbar = () => {
           <div className=' py-6 md:py-0 flex flex-col md:flex-row gap-4'>
           
             
-            <button className='flex items-center gap-1  border rounded-2xl px-8 py-1 font-bold text-blue-600 bg-white hover:bg-blue-600 hover:text-white duration-300'>
+            <button className='flex items-center gap-1  border rounded-2xl px-8 py-1 font-bold text-blue-600 bg-white focus:bg-blue-600 focus:text-white duration-300'>
             <BiHome />
               Home</button>
 
 
-              <button className=' items-center gap-1  border rounded-2xl px-8 py-1 font-bold text-white bg-blue-600 hover:bg-white hover:text-blue-600 duration-300'>
+              {/* <button className=' items-center gap-1  border rounded-2xl px-8 py-1 font-bold text-white bg-blue-600 hover:bg-white hover:text-blue-600 duration-300'>
             
-              Sign Up</button>
+              Sign Up</button> */}
        
 
           </div>
